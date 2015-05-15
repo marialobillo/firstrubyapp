53addf205fd64ed24228c216ef652670f18d8954
Neniah <maria.lobillo.santos@gmail.com>
2015-05-15 13:13:09 +0200

    average

--- analyzer.rb
+++ analyzer.rb
@@ -2,18 +2,24 @@
 text = ''
 line_count = 0
 
-File.open("text.txt").each do
-	|line| line_count += 1
-	text << line
-end
-
-puts "======================================"
-puts "#{line_count} lines"
-total_characters = text.length
-puts "#{total_characters} characters"
-total_characters_nospaces = text.gsub(/\s+/, '').length 
-puts "#{total_characters_nospaces} characters (characters excluding spaces)"
-
+lines = File.readlines("text.txt")
+line_count = lines.size
+text = lines.join
 word_count = text.split.length
-puts "#{word_count} words" 
+character_count = text.length
+character_count_nospaces = text.gsub(/\s+/, '').length
+paragraph_count = text.split(/\n\n/).length
+sentence_count = text.split(/\.|\?|!/).length
+
+puts "#{line_count} lines"
+puts "#{character_count} characters"
+puts "#{character_count_nospaces} characters excluding spaces"
+puts "#{word_count} words"
+puts "#{paragraph_count} paragraphs"
+puts "#{sentence_count} sentences"
+puts "#{sentence_count / paragraph_count} sentences per paragraph (average)"
+puts "#{word_count / sentence_count} words per sentence (average)"
+
+
+
 
